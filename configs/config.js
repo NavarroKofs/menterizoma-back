71b55bd2fefa0ca547f4790f4cb51524c9cfaf91
Roberto Navarro <navarroc.roberto@gmail.com>
2021-01-30 19:40:27 -0600

    V1.0

--- configs/config.js
+++ configs/config.js
@@ -3,7 +3,7 @@ module.exports = {
 		host: '127.0.0.1',
 		port:3306,
 		user: 'root',
-		password: 'Rocm971021',
+		password: 'admin',
 		database: 'seguridad'
 	},
 	CLAVE_SECRETA: "C1av3-53cr37a*",
